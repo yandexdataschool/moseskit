36022bd59820ebc4b94c23733f351b75859a1f16
justheuristic <justheuristic@gmail.com>
2019-01-10 00:08:54 +0300

    remove container after running

--- run_moses.sh
+++ run_moses.sh
@@ -7,15 +7,15 @@ DATA_PATH="${2:-`pwd`}"
 container=justheuristic/vsyo_ty_moses:latest 
 
 if [ "$MODE" == "train_and_translate" ]; then
-    docker run -it -v `realpath $DATA_PATH`:/home/moses/shared $container /home/moses/shared/src/train_and_translate.sh
+    docker run --rm -it -v `realpath $DATA_PATH`:/home/moses/shared $container /home/moses/shared/src/train_and_translate.sh
 elif [ "$MODE" == "train" ]; then
-    docker run -it -v `realpath $DATA_PATH`:/home/moses/shared $container /home/moses/shared/src/train.sh
+    docker run --rm -it -v `realpath $DATA_PATH`:/home/moses/shared $container /home/moses/shared/src/train.sh
 elif [ "$MODE" == "translate" ]; then
-    docker run -it -v `realpath $DATA_PATH`:/home/moses/shared $container /home/moses/shared/src/translate.sh 
+    docker run --rm -it -v `realpath $DATA_PATH`:/home/moses/shared $container /home/moses/shared/src/translate.sh 
 elif [ "$MODE" == "bash" ]; then
-    docker run -it -v `realpath $DATA_PATH`:/home/moses/shared $container bash
+    docker run --rm -it -v `realpath $DATA_PATH`:/home/moses/shared $container bash
 elif [ "$MODE" == "jupyter" ]; then
-    docker run -it -v `realpath $DATA_PATH`:/home/moses/shared -p 8080:8888 $container /home/moses/shared/src/run_jupyter.sh
+    docker run --rm -it -v `realpath $DATA_PATH`:/home/moses/shared -p 8080:8888 $container /home/moses/shared/src/run_jupyter.sh
 else
     echo "unknown mode: $MODE";
 fi
